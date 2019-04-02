d9fa065c7c3b41539fc5312e06487d321104bc8d
xiecong <xiecong@tecpie.com>
2019-04-02 09:37:30 +0800

    去掉无用jar 引用 启动类移动到最外层

--- src/main/java/com/revert/RevertApplication.java
+++ src/main/java/com/revert/RevertApplication.java
@@ -1,10 +1,9 @@
-package com.revert.platform;
+package com.revert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"com.revert"})
 public class RevertApplication extends SpringBootServletInitializer {
