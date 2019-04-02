a221f57abee441ef7f99bb968936ba6d3b22690e
xiecong <xiecong@tecpie.com>
2019-04-02 09:34:33 +0800

    全局参数使用

--- src/main/java/com/revert/platform/common/base/controller/BaseControllerExceptionHandler.java
+++ src/main/java/com/revert/platform/common/base/controller/BaseControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.revert.platform.common.base.controller;
 
 import com.revert.platform.common.base.model.WebResult;
+import com.revert.platform.common.constant.StaticsData;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,9 +12,9 @@ public class BaseControllerExceptionHandler {
 
     private static final WebResult defaultResult = new WebResult();
     static {
-        defaultResult.setCode("5000");
+        defaultResult.setCode(StaticsData.CODE_ERROR);
         defaultResult.setData(null);
-        defaultResult.setMessage("系统繁忙，请稍后再试");
+        defaultResult.setMessage(StaticsData.MSG_ERROR);
     }
 
     /**
